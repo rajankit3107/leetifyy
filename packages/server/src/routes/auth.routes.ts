cb7a7c67881d538d557e66b569142a306169d5f9
rajankit3107 <e22cseu0331@bennett.edu.in>
2025-10-31 16:23:37 +0530

    fix : fixed routes

--- packages/server/src/routes/auth.routes.ts
+++ packages/server/src/routes/auth.routes.ts
@@ -1,11 +1,12 @@
 import express from 'express';
-import { login, register } from '../controllers/auth.controller';
+import { login, logout, register } from '../controllers/auth.controller';
+import { authMiddleware } from '../middlewares/auth.middleware';
 
 const authRoutes = express.Router();
 
 authRoutes.post('/register', register);
 authRoutes.post('/login', login);
-// authRoutes.post('/logout');
+authRoutes.post('/logout', authMiddleware, logout);
 // authRoutes.get('/check');
 
 export default authRoutes;
